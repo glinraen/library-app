49cf58142cc0fc2ef6a8dca8ae3b7eb532e9b484
GLINRAEN <MISTRESS.WYN@GMAIL.COM>
2016-06-20 00:04:01 -0700

    adds views

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -4,4 +4,20 @@ class UsersController < ApplicationController
     	render :index
   	end
 
+  	def new
+  	 	@user = User.new
+    	render :new
+  	end
+
+  	def create
+  		user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+  		@user = User.create(user_params)
+  		redirect_to "/users"
+  	end
+
+  	def show
+  		@user = User.find(params[:id])
+  		render :show
+  	end
+
 end
--- lib_app/config/routes.rb
+++ lib_app/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: "users#index"
 
   get "/users", to: "users#index", as: "users"
-
   get "/users/new", to: "users#new", as: "new_user"
+  post "/users", to: "users#create"
+  get "/users/:id", to: "user#show"
+
 end
